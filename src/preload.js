f2e606c36df91081422bbccabd6165477534889f
programmingKyle <155344874+programmingKyle@users.noreply.github.com>
2024-02-13 18:45:40 -0500

    Added Window Frame
    - Disabled default window frame
    - Added custom one

--- src/preload.js
+++ src/preload.js
@@ -1,5 +1,5 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('api', {
-
+    frameHandler: (data) => ipcRenderer.invoke('frame-handler', data),
 });
